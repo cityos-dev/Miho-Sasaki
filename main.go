5503dfd2456eb694f5c8051efded31cf4f1efc7d
MihoSasaki <miho.sprit@gmail.com>
2023-02-14 11:25:56 +0900

    setup inital go project

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+
+	"videoservice/handler"
+	"videoservice/infra"
+	"videoservice/service"
+)
+
+func main() {
+	g := gin.Default()
+
+	engine, err := infra.Init()
+	if err != nil {
+		log.Fatalf("error occurs when setting up mysql: %v", err)
+	}
+
+	factory := service.NewVideoService(infra.NewVideDatabase(engine),
+		infra.NewFileServer("/video"))
+
+	g.Use(service.SetFactoryMiddleware(factory))
+
+	log.Println("start server...")
+	r := g.Group("/v1")
+	RegisterHandlersWithOptions(r, handler.NewServerHandler())
+
+	log.Fatal(g.Run(":8080"))
+}
+
+// RegisterHandlersWithOptions creates a handler
+func RegisterHandlersWithOptions(rg *gin.RouterGroup, si handler.ServerHandler) {
+	rg.GET("/files", si.GetFiles)
+
+	rg.POST("/files", si.PostFiles)
+
+	rg.DELETE("/files/:id", si.DeleteFilesFileId)
+
+	rg.GET("/files/:id", si.GetFilesFileId)
+
+	rg.GET("/health", si.GetHealth)
+}
